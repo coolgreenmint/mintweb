49ae6973f7ca0d3663872327f89ae18c9a37cdbb
coolgreenmint <sanguolian_it@163.com>
2021-09-16 10:52:16 +0800

    Initial commit

---
+++ src/main/java/org/mint/mintweb/MintWebApplication.java
@@ -0,0 +1,17 @@
+package org.mint.mintweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: MintWebApplication
+ * @Description: springboot项目主类
+ * @author: mint
+ * @date: 2021/9/16 10:33
+ */
+@SpringBootApplication
+public class MintWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MintWebApplication.class, args);
+    }
+}
---
+++ src/test/java/org/mint/mintweb/MintwebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mint.mintweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
